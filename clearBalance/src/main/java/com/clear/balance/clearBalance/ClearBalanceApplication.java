fb03652a179561cc59f1d6305ffaaa05902a9ef2
pagarciaSim <pablo.garciasimavilla@gmail.com>
2025-11-03 07:52:44 +0100

    First commit

---
+++ clearBalance/src/main/java/com/clear/balance/clearBalance/ClearBalanceApplication.java
@@ -0,0 +1,13 @@
+package com.clear.balance.clearBalance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClearBalanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClearBalanceApplication.class, args);
+	}
+
+}
