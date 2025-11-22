7c33d8daed78e6eea2ebfe020b29a82000255ade
pagarciaSim <pablo.garciasimavilla@gmail.com>
2025-11-22 18:05:56 +0100

    Frontend / stats & navbar html & design

--- clearBalanceApp/src/app/component/navbar/navbar.component.ts
+++ clearBalanceApp/src/app/component/navbar/navbar.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import { TooltipService } from 'src/app/service/tooltip.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements AfterViewInit {
+
+  constructor(private tooltipService: TooltipService) {}
+
+  ngAfterViewInit(): void {
+    this.tooltipService.initialize();
+  }
+
+  logOut() {
+    throw new Error('Method not implemented.');
+  }
 
 }
